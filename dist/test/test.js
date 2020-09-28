0d9c7aae44cc6f55eaafea18a2994630cad3c723
User <user@Lawrence.local>
2020-09-28 11:38:14 +0100

    test

--- dist/test/test.js
+++ dist/test/test.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var supertest_1 = __importDefault(require("supertest"));
+var mongoose_1 = __importDefault(require("mongoose"));
 var app_1 = __importDefault(require("../app"));
 var request = supertest_1.default(app_1.default);
 describe("/", function () {
@@ -75,4 +76,9 @@ describe("/", function () {
             done();
         });
     });
+});
+afterAll(function (done) {
+    // Closing the DB connection allows Jest to exit successfully.
+    mongoose_1.default.connection.close();
+    done();
 });
